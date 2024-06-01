1445f14c3d3ed7b0d9bfd07fb9f56b59d140b07c
peterermakow <peterinno@yandex.ru>
2024-06-01 22:39:40 +0300

    first commit

---
+++ limitmodule/src/test/java/ru/ermakow/limitmodule/LimitmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ermakow.limitmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitmoduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
